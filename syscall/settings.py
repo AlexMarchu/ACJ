e5e2c4573fbfce2337fcef3cb4b65f18640e5435
AlexMarchu <150007881+AlexMarchu@users.noreply.github.com>
2024-12-21 00:17:46 +1000

    rename

--- syscall/settings.py
+++ syscall/settings.py
@@ -30,7 +30,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'dalamber.urls'
+ROOT_URLCONF = 'syscall.urls'
 
 TEMPLATES = [
     {
@@ -48,7 +48,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'dalamber.wsgi.application'
+WSGI_APPLICATION = 'syscall.wsgi.application'
 
 
 DATABASES = {
