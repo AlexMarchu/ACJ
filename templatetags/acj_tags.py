ff1e54f95a16992e1e9cc98bd69ce075e0f22383
sxmurxy2005 <pashashaxmatov2005@mail.ru>
2024-12-30 20:40:52 +1000

    start stylizing contest

---
+++ templatetags/acj_tags.py
@@ -0,0 +1,12 @@
+from django import template
+from datetime import datetime
+
+register = template.Library()
+
+@register.simple_tag
+def hours_between(start_date, end_date):
+    delta = end_date - start_date
+    hours, r = divmod(delta.total_seconds(), 3600)
+    minutes = r // 60
+
+    return f'{int(hours):02}:{int(minutes):02}'
